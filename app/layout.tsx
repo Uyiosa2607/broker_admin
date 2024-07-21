873a904fe1aa7010aa24f0ded5e62a170df1159f
osas4real <aghahowaosamudiamen03@gmail.com>
2024-07-21 15:42:16 +0100

    initial commit for broker_admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
-import { Inter as FontSans } from "next/font/google";
-import { Toaster } from "@/components/ui/toaster";
-
-import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Users Management",
   description: "Hobby Project",
 };
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export default function RootLayout({ children, session }: any) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <div className="container w-full mx-auto px-[20px]">
-          <SessionProvider session={session}>
-            {children}
-            <Toaster />
-          </SessionProvider>
-        </div>
+      <body>
+        <div>{children}</div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div className="container w-[100px] mx-auto  lg:w-[500px] flex flex-col gap-5 mt-[100px]">
-        <h1 className="text-center  font-bold ">Welcome</h1>
-        <Button asChild>
-          <Link href="/login">Login here</Link>
-        </Button>
-      </div>
+      <h1>This is home</h1>
     </main>
   );
 }
