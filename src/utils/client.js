4e1630b62cf55c1711b5e1e4b0abe7af9ece0909
osas4real <aghahowaosamudiamen03@gmail.com>
2024-08-07 12:10:24 +0100

    removed keys from file

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,9 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  "https://tlhcxgvltgfjujhybxpg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsaGN4Z3ZsdGdmanVqaHlieHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5OTkzMTIsImV4cCI6MjAyMDU3NTMxMn0.QhNlIpTCBfYBfbUIbEIEwdz1ey3H4Is-KJLTCA1UGPc"
-);
-
+//file in env
 
 export default supabase;
