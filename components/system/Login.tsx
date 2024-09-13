50954976f5af4915cbc957bf95573a86605b925b
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2024-09-14 00:31:18 +0100

    fixed major bugs

--- components/system/Login.tsx
+++ components/system/Login.tsx
@@ -22,16 +22,20 @@ export default function Login() {
     const password: any = formdata.get("password");
 
     setLoading(true);
-    const { error } = await supabase.auth.signInWithPassword({
+    const { error }: any = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
     });
-    if (error?.code === "invalid_credentials")
+    if (error) {
       toast({
         variant: "destructive",
-        title: "Wrong email or password",
-        description: "Please check login credentials and try again",
+        title: "Unable to sign in",
+        description: error?.message,
       });
+      setLoading(false);
+      return;
+    }
+
     toast({
       description: "Login Approved",
     });
